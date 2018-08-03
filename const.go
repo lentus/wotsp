11083f5a8b3d1806e35e17ec6b38a3c677b0448c
Jan <jan@artemisc.eu>
2018-08-03 02:00:32 +0200

    updated constants

--- const.go
+++ const.go
@@ -2,15 +2,15 @@ package wotsp
 
 // constants for WOTPS signatures for both W4 and W16 modes.
 const (
-	W4PublicKeyBytes = 0
-	W4SecretKeyBytes = 0
-	W4Bytes          = 0
+	W4PublicKeyBytes = 2144        // size of publis key
+	W4SecretKeyBytes = W4SeedBytes // TODO should this be the size of the internal private key, or the seed?
+	W4Bytes          = 2144        // size of signatures
 	W4SeedBytes      = 32
 	W4PubSeedBytes   = 32
 
-	W16PublicKeyBytes = 0
-	W16SecretKeyBytes = 0
-	W16Bytes          = 0
+	W16PublicKeyBytes = 2144
+	W16SecretKeyBytes = W16SeedBytes
+	W16Bytes          = 4256
 	W16SeedBytes      = 32
 	W16PubSeedBytes   = 32
 )
